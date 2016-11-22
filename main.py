2b296a223e66354846c83feea15766cae6ac4db3
wendeljunior <wendeljazz@gmail.com>
2016-11-21 23:28:57 -0300

    Inicio do estagio de cadastramento de novo evento.

--- main.py
+++ main.py
@@ -110,6 +110,98 @@ def loginTestador():
 	driver.execute_script("document.getElementsByClassName('success')[0].click()")
 
 
+def insereTituloEvento(title):
+	driver.find_element_by_id("id_title").send_keys(title)
+
+def insereDescricaoEvento(descricao):
+	driver.find_element_by_id("mceu_25").send_keys(descricao)
+
+def clickNegrito():
+	driver.execute_script("document.getElementsByTagName('button')[1].click();")
+
+def clickItalic():
+	driver.execute_script("document.getElementsByTagName('button')[2].click()")
+
+def clickSublinhado():
+	driver.execute_script("document.getElementsByTagName('button')[3].click()")
+
+def configColor():
+	driver.find_element_by_class_name("mce-open") #Abre paleta de cores
+	driver.find_element_by_id('mceu_30-16').click() #Escolhe vermelho
+	driver.execute_script("document.getElementsByTagName('button')[4].click()") #Clica para acionar cor
+
+def alinhaEsquerda():
+	driver.execute_script("document.getElementsByTagName('button')[6].click()")
+
+def alinhaCentro():
+	driver.execute_script("document.getElementsByTagName('button')[7].click()")
+
+def alinhaDireita():
+	driver.execute_script("document.getElementsByTagName('button')[8].click()")
+
+def desfazTexto():
+	driver.execute_script("document.getElementsByTagName('button')[11].click()")
+
+def refazTexto():
+	driver.execute_script("document.getElementsByTagName('button')[12].click()")
+
+def limparFormatacao():
+	driver.execute_script("document.getElementsByTagName('button')[13].click()")
+
+def titulacao(): #Headings
+	driver.execute_script("document.getElementsByTagName('button')[14].click()")
+	driver.execute_script("document.getElementById('mceu_47-text').click()")
+	time.sleep(2)	
+	driver.execute_script("document.getElementById('mceu_48-text').click()")
+	time.sleep(2)	
+	driver.execute_script("document.getElementById('mceu_49-text').click()")
+	time.sleep(2)	
+	driver.execute_script("document.getElementById('mceu_50-text').click()")
+	time.sleep(2)	
+	driver.execute_script("document.getElementById('mceu_51-text').click()")
+	time.sleep(2)	
+	driver.execute_script("document.getElementById('mceu_52-text').click()")
+	time.sleep(2)	
+	driver.execute_script("document.getElementById('mceu_53-text').click()")
+
+def tamanhoFonte(): #Set font size
+	driver.execute_script("document.getElementById('mceu_13-open').click()")
+	driver.execute_script("document.getElementById(mceu_55-text).click()")
+	time.sleep(2)
+	driver.execute_script("document.getElementById(mceu_56-text).click()")
+	time.sleep(2)
+	driver.execute_script("document.getElementById(mceu_57-text).click()")
+	time.sleep(2)
+	driver.execute_script("document.getElementById(mceu_58-text).click()")
+	time.sleep(2)
+	driver.execute_script("document.getElementById(mceu_59-text).click()")
+	time.sleep(2)
+	driver.execute_script("document.getElementById(mceu_60-text).click()")
+	time.sleep(2)
+	driver.execute_script("document.getElementById(mceu_61-text).click()")
+
+def clickPreVisualizar(): #clica no botao preview
+	driver.execute_script("document.getElementsByTagName('button')[17].click()") #Preview
+	time.sleep(4)
+	driver.find_element_by_class_name("mce-close").click() #Close Preview
+
+def publish(check): #marca ou desmarca a opcao publish
+	if check == True:
+		driver.execute_script("document.getElementById('uniform-id_is_published').childNodes[0].className = 'checked';")
+	else:
+		driver.execute_script("document.getElementById('uniform-id_is_published').childNodes[0].className = '';")
+
+
+def allowPublicVting(check): #marca ou desmarca a opcao Allow Public Voting
+	if check == True:
+		driver.execute_script("document.getElementById('uniform-id_allow_public_voting').childNodes[0].className = 'checked';")
+	else:
+		driver.execute_script("document.getElementById('uniform-id_allow_public_voting').childNodes[0].className = '';")
+
+def configData(): #Faz todo o proceso para definir a data do evento
+	driver.find_element_by_id("id_due_date_0").click() #clica em Due Date para poder setar data
+	
+
 
 
 comecar()
